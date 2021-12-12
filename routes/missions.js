2aa8f7f6146a59b52c98da9175a78b69ff5a32f6
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-12-12 14:55:44 +0400

    Customized post and get api
    Tested with Post Man

--- routes/missions.js
+++ routes/missions.js
@@ -1,18 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const Mission = require('../model/Mission');
 
 // @route   POST api/missions
 // @desc    Add a Mission
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Add a mission');
+router.post('/', async (req, res) => {
+  console.log('Mission entered');
+  const { name, tasks } = req.body;
+
+  try {
+    let mission = new Mission({ name, tasks });
+    await mission.save();
+
+    res.json(mission);
+    console.log('Mission Added');
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Server Error');
+  }      
+  // res.send('Add a mission');
 });
 
 // @route   GET api/missions
 // @desc    Get Missions
 // @access  Public
-router.get('/', (req, res) => {
-  res.send('List Missions');
-});
+router.get(
+  '/',
+  async (req, res) => {
+    try {
+      // get all missions
+      const missions = await Mission.find({}).sort({_id: -1});
+      res.json(missions);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
