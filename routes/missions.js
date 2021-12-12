299c1334a0f241a288b415082a343fded2eda223
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-12-12 12:01:49 +0400

    Bring the routes to server.js

--- routes/missions.js
+++ routes/missions.js
@@ -8,4 +8,11 @@ router.post('/', (req, res) => {
   res.send('Add a mission');
 });
 
+// @route   GET api/missions
+// @desc    Get Missions
+// @access  Public
+router.get('/', (req, res) => {
+  res.send('List Missions');
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const missions = require('./routes/missions');
 
 const app = express();
 app.get('/', (req, res) => res.json({msg: "Mission task"}));
+
+// Define Routes
+app.use('/api/missions', missions);
 
 const PORT = process.env.PORT || 5000;
 
