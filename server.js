3bd86f1ae1fcc4836fe6d71cc882ba6c8434e93b
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-12-12 12:25:34 +0400

    Bring db.js into server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const missions = require('./routes/missions');
 
 const app = express();
+
+// Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.json({msg: "Mission task"}));
 
 // Define Routes
