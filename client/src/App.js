914d54e6c754de52337788faff84ce6ae2aa58ed
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-12-14 11:47:39 +0400

    App js and css update - upto Mission Lists/New/Edit/Update/Delete Feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Missions from './components/missions/Missions';
+import Mission from './components/missions/Mission';
+import MissionForm from './components/missions/MissionForm';
+import MissionState from './context/mission/MissionState';
+import Alert from './components/layout/Alert';
+import Back from './components/layout/Back';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <MissionState>
+      <Router>
+        <div className="container">
+          <div className="main_container">
+            <Back />
+            <Alert />
+            <Routes>
+              <Route exact path='/' element={<Missions />} />           
+              <Route exact path='/api/missions/:id' element={<Mission />} />           
+              <Route exact path='/missions/new' element={<MissionForm />} />           
+            </Routes>
+          </div>
+        </div>        
+      </Router>
+    </MissionState>
   );
 }
 
